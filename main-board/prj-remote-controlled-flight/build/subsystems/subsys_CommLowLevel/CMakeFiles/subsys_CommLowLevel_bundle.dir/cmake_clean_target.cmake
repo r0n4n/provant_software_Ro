c9e4cc60673fc7fb81b4d1455ca9d4f7c807f5f3
Exxu <ra.andrade.alfaro@gmail.com>
2015-07-28 10:15:53 -0300

    eliminado build

--- main-board/prj-remote-controlled-flight/build/subsystems/subsys_CommLowLevel/CMakeFiles/subsys_CommLowLevel_bundle.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../../subsystems/subsys_CommLowLevel/build/libsubsys_CommLowLevel_bundle.a"
-)
--- main-board/prj-remote-controlled-flight/build/subsystems/subsys_ContinuousControl/CMakeFiles/subsys_ContinuousControl_bundle.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../../subsystems/subsys_ContinuousControl/build/libsubsys_ContinuousControl_bundle.a"
-)
